4f9a4309c9d8722284277fa63e37594d9e80f1e7
Padmavathy G <96161598+G-Padmavathy@users.noreply.github.com>
2023-10-20 12:47:06 +0530

    Update TaskManagementApp.java

--- TaskManagementApp.java
+++ TaskManagementApp.java
@@ -8,25 +8,34 @@ public class TaskManagementApp {
 	static Scanner sc = new Scanner(System.in);
 	ArrayList<Task> taskList = new ArrayList<Task>();
 	int taskIdCounter = 1;
-
 	TaskManagementApp(){
-		taskList.add(new Task(taskIdCounter,"Mini Project" ,false,LocalDate.now(),"High"));
+		taskList.add(new Task(taskIdCounter,"Mini Project" ,false,LocalDate.now(),"Medium"));
 		taskIdCounter++;
-		taskList.add(new Task(taskIdCounter,"Module completion" ,false,LocalDate.now(),"low"));
+		taskList.add(new Task(taskIdCounter,"Module Completion" ,false,LocalDate.now(),"Low"));
+		taskIdCounter++;
+		taskList.add(new Task(taskIdCounter,"Assessment" ,false,LocalDate.now(),"High"));
 		taskIdCounter++;
 	}
 	
+	
 	 public void viewAllTasks() {
-	        System.out.println("========================================================");
-	        if (taskList.isEmpty()) {
-	            System.out.println("No tasks found.");
-	        } else {
+		 System.out.println("========================================================");
+	     if (taskList.isEmpty()) {
+	    	 System.out.println("No tasks found.");
+	         } else {
 	            for (Task task : taskList) {
-	                String completionStatus = task.isCompleted() ? "Completed" : "Not Completed";
+	            	String completionStatus;
+	            	if (task.isCompleted()) {
+	            	    completionStatus = "Completed";
+	            	} else {
+	            	    completionStatus = "Not Completed";
+	            	}
 	                System.out.println("Task ID: " + task.getTaskId());
 	                System.out.println("Description: " + task.getTaskDescription());
 	                System.out.println("Due Date: " + task.getDueDate());
+	                System.out.println("Priority:"+ task.getPriority());
 	                System.out.println("Status: " + completionStatus);
+	                
 	                System.out.println();
 	            }
 	        }
@@ -37,6 +46,9 @@ public class TaskManagementApp {
 	 public void addTask() {
 		    System.out.println("Enter task description: ");
 		    String taskDescription = sc.nextLine();
+		    if (taskDescription.isEmpty()) {
+		    	System.out.println("Your Description is Empty.");
+		    }
 		    System.out.println("Enter due date (YYYY-MM-DD): ");
 		    String inputDueDate = sc.nextLine();
 		    LocalDate dueDate;
@@ -51,6 +63,47 @@ public class TaskManagementApp {
 		    taskList.add(task);
 		    taskIdCounter++;
 		}
+	 
+	 
+	 	public void updateTaskDetails() {
+	        System.out.println("Enter task ID to update details: ");
+	        int taskId = sc.nextInt();
+	        sc.nextLine();  
+
+	        for (Task task : taskList) {
+	            if (task.getTaskId() == taskId) {
+	                System.out.println("Enter new task description (or press Enter to keep the current description): ");
+	                String newDescription = sc.nextLine();
+	                if (!newDescription.isEmpty()) {
+	                    task.setTaskDescription(newDescription);
+	                }
+
+	                System.out.println("Enter new due date (YYYY-MM-DD) (or press Enter to keep the current due date): ");
+	                String inputDueDate = sc.nextLine();
+	                if (!inputDueDate.isEmpty()) {
+	                    LocalDate newDueDate = LocalDate.parse(inputDueDate);
+	                    task.setDueDate(newDueDate);
+	                }
+
+	                System.out.println("Enter new priority (High/Medium/Low) (or press Enter to keep the current priority): ");
+	                String newPriority = sc.nextLine();
+	                if (!newPriority.isEmpty()) {
+	                    task.setPriority(newPriority);
+	                }
+	                
+	                System.out.println("Do you want to mark the task as completed? (Y-Yes / N-No): ");
+	                String markAsCompletedChoice = sc.nextLine();
+	                if (markAsCompletedChoice.equalsIgnoreCase("Y")) {
+	                    task.markAsCompleted();
+	                }
+
+	                System.out.println("Task details updated successfully.");
+	                return;
+	            }
+	        }
+
+	        System.out.println("Task with that ID does not exist.");
+	    }
 
 
 	    public void markTaskAsCompleted() {
@@ -77,7 +130,7 @@ public class TaskManagementApp {
 	            }
 	        }
 	    }
-
+	    
 
 	    public void deleteTask() {
 	        System.out.println("Enter task ID to delete: ");
@@ -91,4 +144,21 @@ public class TaskManagementApp {
 	        }
 	        System.out.println("Task with that ID does not exist.");
 	    }
+	    
+	    public void deleteAllTask()
+		{
+			System.out.println("==================================================================================================");
+
+			if(taskList.size()>0)
+			{
+				taskList.clear();
+				System.out.println("All records deleted successfully!!");
+			}
+			else
+			{
+				System.out.println("No record available!!");
+			}
+			System.out.println("==================================================================================================");
+
+		}
 	}
